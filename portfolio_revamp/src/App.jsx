0ecb84024793f247975265b2eac13d7d5f06f838
ekaagra gupta <ekaagrag2006@gmail.com>
2026-01-30 12:03:04 +0530

    kiksj

--- portfolio_revamp/src/App.jsx
+++ portfolio_revamp/src/App.jsx
@@ -1,22 +1,3 @@
-import { Canvas } from "@react-three/fiber"
-import { OrbitControls } from "@react-three/drei"
-
-function Box() {
-  return (
-    <mesh>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="orange" />
-    </mesh>
-  )
-}
-
 export default function App() {
-  return (
-    <Canvas style={{ width: "100vw", height: "100vh" }}>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[5, 5, 5]} />
-      <Box />
-      <OrbitControls />
-    </Canvas>
-  )
+  return <h1>Hello React</h1>
 }
