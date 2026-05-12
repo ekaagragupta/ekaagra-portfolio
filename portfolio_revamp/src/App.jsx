d7776b7ac3654e559ffc8056c06c207ded5b884c
ekaagra gupta <ekaagrag2006@gmail.com>
2026-05-12 18:16:48 +0530

    “changes”

--- portfolio_revamp/src/App.jsx
+++ portfolio_revamp/src/App.jsx
@@ -2,10 +2,7 @@ import { motion } from 'framer-motion';
 import { useState, useEffect } from 'react';
 import './App.css';
 import LetterGlitch from './components/LetterGlitch';
-
-
-
-
+// import LiquidEther from './LiquidEther';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -13,35 +10,36 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 10000);
+    }, 3000);
     return () => clearTimeout(timer);
   }, []);
 
   // If we are loading, show the Splash Screen!
   if (isLoading) {
     return (
-      <div className="loading-container" style={{ position: 'relative' }}>
+      <div className="relative w-screen h-screen flex flex-col items-center justify-center overflow-hidden bg-[#0f172a]">
 
         {/* BACKGROUND GLITCH ANIMATION */}
         <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 0 }}>
           <LetterGlitch glitchSpeed={50} centerVignette={true} outerVignette={false} smooth={true} />
         </div>
 
-        {/* YOUR TEXT ON TOP */}
+
         <div style={{ position: 'relative', zIndex: 10, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <motion.h1
             className="title"
+            style={{ fontSize: '2.5rem' }}
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 1 }}
           >
-            hey user
+            Retrieving intelligence
           </motion.h1>
 
           <motion.p
             animate={{ opacity: [0.5, 1, 0.5] }}
             transition={{ duration: 1, repeat: Infinity }}
-            style={{ marginTop: '2rem', color: '#ff8a00' }}
+            style={{ marginTop: '2rem', color: '#f7f9f8ff' }}
           >
             Loading...
           </motion.p>
@@ -55,6 +53,28 @@ function App() {
   // If loading is finished, show the Main Portfolio!
   return (
     <div className="app-container">
+      {/* <div style={{ width: '100%', height: 600, position: 'relative' }}>
+        <LiquidEther
+          colors={['#5227FF', '#FF9FFC', '#B497CF']}
+          mouseForce={20}
+          cursorSize={100}
+          isViscous
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo
+          autoSpeed={0.5}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={3000}
+          autoRampDuration={0.6}
+          color0="#5227FF"
+          color1="#FF9FFC"
+          color2="#B497CF"
+        />
+      </div> */}
       <motion.h1
         className="title"
         initial={{ opacity: 0, y: -20 }}
